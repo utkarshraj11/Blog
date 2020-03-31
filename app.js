e7ff53cb71c69f1f8179454826fed61145ba8ea7
utkarsh raj <utkarsh.alok26@gmail.com>
2020-04-01 04:15:57 +0530

    Chnaged listening port

--- app.js
+++ app.js
@@ -80,6 +80,6 @@ const requestedPostId = req.params.postId;
 
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
 });
